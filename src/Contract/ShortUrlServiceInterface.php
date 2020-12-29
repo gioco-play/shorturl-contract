abd3726d83f84e8257f5697a908e59686b0a0a41
Xuan Yang <xuan.yang@goldenf.co>
2020-12-29 15:43:17 +0800

    first commit

---
+++ src/Contract/ShortUrlServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\ShortUrl\Contract;
+/**
+ * 交易
+ */
+interface ShortUrlServiceInterface {
+
+    /**
+     * 短網址產生
+     *
+     * @param string $gameUrl
+     * @param string $operatorCode
+     * @param string $playerName
+     * @param string $gameCode
+     * @return string
+     */
+    function gen(string $gameUrl, string $operatorCode, string $playerName, string $gameCode): string;
+}
+
